517f9735461c6e143c23404da060cbd379ec366c
thanh241203 <thanhkon241203@gmail.com>
2025-04-11 14:26:47 +0700

    bai 2

--- algthor/src/com/company/bai1.java
+++ algthor/src/com/company/bai1.java
@@ -6,11 +6,13 @@ import java.util.List;
 public class bai1 {
     static List<Integer> getIntegers(int a[]) {
         ArrayList<Integer> integers = new ArrayList<>();
-        for (int i = 0; i < a.length; i+=2) {
+        for (int i = 0; i < a.length; i += 2) {
             integers.add(a[i]);
         }
         return integers;
     }
+
+
 
     public static void main(String[] args) {
         int a[] = {10, 20, 30, 40, 50};
---
+++ algthor/src/com/company/bai2.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class bai2 {
+    static Integer getIntegerIndex(int a[], int value) {
+        for (int i = 0; i < a.length; i++) {
+           if(a[i]==value){
+               return  i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int array[] = {10, 20, 30, 40, 50};
+        int currentValue = 60;
+        int b = getIntegerIndex(array, currentValue);
+        System.out.println(b);
+    }
+}
